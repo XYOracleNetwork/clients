729e2c9c1a94105301d08480e938399a53b7e0dc
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-12-06 08:56:03 -0600

    Add NFT Index Manifest to Public Child Nodes

--- packages/node/packages/express/packages/dependencies/src/Manifest/PublicChildren/index.ts
+++ packages/node/packages/express/packages/dependencies/src/Manifest/PublicChildren/index.ts
@@ -2,6 +2,8 @@ import { PackageManifestPayload } from '@xyo-network/manifest-model'
 
 import imageThumbnailNodeManifest from './imageThumbnailNode.json'
 import nftContractNodeManifest from './nftContractNode.json'
+import nftIndexNodeManifest from './nftIndexNode.json'
 
 export const imageThumbnailNode = imageThumbnailNodeManifest as PackageManifestPayload
 export const nftContractNode = nftContractNodeManifest as PackageManifestPayload
+export const nftWitnessIndexNode = nftIndexNodeManifest as PackageManifestPayload
