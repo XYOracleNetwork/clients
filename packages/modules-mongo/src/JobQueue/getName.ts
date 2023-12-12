644768ed88085bfff0b0455cdc79db7efd147376
Arie Trouw <arie.trouw@xylabs.com>
2023-12-11 17:22:54 -0800

    couple cleanups

--- packages/modules-mongo/src/JobQueue/getName.ts
+++ packages/modules-mongo/src/JobQueue/getName.ts
@@ -1,7 +1,7 @@
-import { Crypto } from '@xylabs/crypto'
+import {v4 as uuid} from 'uuid'
 
 // TODO: When running in AWS use Task Metadata Endpoint to get Task ID
 // https://docs.aws.amazon.com/AmazonECS/latest/userguide/task-metadata-endpoint-v4-fargate.html
 export const getName = (): string => {
-  return Crypto.randomUUID()
+  return uuid()
 }
