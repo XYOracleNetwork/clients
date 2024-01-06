751d0c7a9a7f8585c3e9f1e370a2bc6db80c80a2
Joel Carter <JoelBCarter@users.noreply.github.com>
2024-01-06 15:39:41 -0600

    Add NFT Metadata Node

--- packages/node/packages/express/packages/dependencies/src/configureMemoryNode.ts
+++ packages/node/packages/express/packages/dependencies/src/configureMemoryNode.ts
@@ -10,7 +10,7 @@ import { NodeInstance } from '@xyo-network/node-model'
 import { readFile } from 'fs/promises'
 import { Container } from 'inversify'
 
-import { nftContractNode } from './Manifest'
+import { nftContractNode, nftMetadataNode } from './Manifest'
 import defaultNode from './node.json'
 import { witnessNftCollections } from './witnessNftCollections'
 
@@ -50,7 +50,7 @@ const loadNodeFromConfig = async (container: Container, config?: string) => {
     : (defaultNode as PackageManifestPayload)
   // TODO: Import all public children from manifest once we're OK to move the image thumbnail
   // modules from the main node to a child node
-  const manifestPublicChildren: PackageManifestPayload[] = config ? [] : [nftContractNode]
+  const manifestPublicChildren: PackageManifestPayload[] = config ? [] : [nftContractNode, nftMetadataNode]
   const mnemonic = container.get<string>(TYPES.AccountMnemonic)
   const wallet = await HDWallet.fromPhrase(mnemonic)
   const locator = container.get<ModuleFactoryLocator>(TYPES.ModuleFactoryLocator)
