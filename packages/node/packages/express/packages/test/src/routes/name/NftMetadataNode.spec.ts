038b1f157f563bf0bd393d53b16b32dda6873998
Joel Carter <JoelBCarter@users.noreply.github.com>
2024-01-06 18:38:06 -0600

    Simplify tests

--- packages/node/packages/express/packages/test/src/routes/name/NftMetadataNode.spec.ts
+++ packages/node/packages/express/packages/test/src/routes/name/NftMetadataNode.spec.ts
@@ -1,16 +1,20 @@
 import { delay } from '@xylabs/delay'
+import { ArchivistInstance } from '@xyo-network/archivist-model'
 import { Bridge } from '@xyo-network/bridge-model'
 import { DivinerInstance } from '@xyo-network/diviner-model'
 import { Payload } from '@xyo-network/payload-model'
 import { SentinelInstance } from '@xyo-network/sentinel-model'
 
-import { getBridgeToChildNode, getDivinerByNameFromChildNode, getSentinelByNameFromChildNode } from '../../testUtil'
+import { getArchivistByNameFromChildNode, getBridgeToChildNode, getDivinerByNameFromChildNode, getSentinelByNameFromChildNode } from '../../testUtil'
 
 const nodeName = 'NftMetadataNode'
 const sentinelName = 'NftMetadataSentinel'
 const indexDivinerName = 'NftMetadataIndexDiviner'
+const archivistName = 'NftMetadataArchivist'
 
 describe(`${nodeName}`, () => {
+  // Gutter Cats
+  const uri = 'https://gutter-cats-metadata.s3.us-east-2.amazonaws.com/metadata/1347'
   let bridge: Bridge
   beforeAll(async () => {
     bridge = await getBridgeToChildNode(nodeName)
@@ -23,11 +27,7 @@ describe(`${nodeName}`, () => {
       expect(sentinel).toBeDefined()
     })
     it('queries NFT metadata URI', async () => {
-      const query = {
-        schema: 'network.xyo.api.call',
-        // Gutter Cats
-        uri: 'https://gutter-cats-metadata.s3.us-east-2.amazonaws.com/metadata/1347',
-      } as Payload
+      const query = { schema: 'network.xyo.api.call', uri }
       const result = await sentinel.report([query])
       expect(result).toBeArray()
       const response = result.filter((p) => p.schema === 'network.xyo.api.call.result')
@@ -36,19 +36,20 @@ describe(`${nodeName}`, () => {
   })
   describe(`${indexDivinerName}`, () => {
     let diviner: DivinerInstance
+    let archivist: ArchivistInstance
     beforeAll(async () => {
       await delay(300) // Allow enough time for index to propagate
       diviner = await getDivinerByNameFromChildNode(indexDivinerName, nodeName)
       expect(diviner).toBeDefined()
+      archivist = await getArchivistByNameFromChildNode(archivistName, nodeName)
+      expect(archivist).toBeDefined()
     })
     it('queries NFT metadata URI', async () => {
-      const query = {
-        schema: 'network.xyo.diviner.payload.query',
-        uri: 'https://gutter-cats-metadata.s3.us-east-2.amazonaws.com/metadata/1347',
-      }
-      const result = (await diviner.divine([query])) as unknown as Payload<{ address?: string; chainId?: number; tokenId?: string }>[]
+      const query = { schema: 'network.xyo.diviner.payload.query', uri }
+      const result = (await diviner.divine([query])) as unknown as Payload<{ uri: string }>[]
       expect(result).toBeDefined()
       expect(result).toBeArrayOfSize(1)
+      expect(result[0].uri).toBe(uri)
     })
   })
 })
--- packages/node/packages/express/packages/test/src/testUtil/Archivist/getArchivist.ts
+++ packages/node/packages/express/packages/test/src/testUtil/Archivist/getArchivist.ts
@@ -2,7 +2,7 @@ import { AccountInstance } from '@xyo-network/account-model'
 import { ArchivistInstance, asArchivistInstance } from '@xyo-network/archivist-model'
 import { ArchivistWrapper } from '@xyo-network/archivist-wrapper'
 
-import { getModuleByName } from '../Node'
+import { getModuleByName, getModuleByNameFromChildNode } from '../Node'
 
 /**
  * @deprecated Use getArchivistByName instead
@@ -15,4 +15,8 @@ export const getArchivist = async (account?: AccountInstance): Promise<Archivist
 export const getArchivistByName = async (name: string = 'Archivist', account?: AccountInstance): Promise<ArchivistInstance> => {
   const archivist = asArchivistInstance(await getModuleByName(name), 'Failed to cast archivist')
   return account ? ArchivistWrapper.wrap(archivist, account) : archivist
+}
+
+export const getArchivistByNameFromChildNode = async (name: string, childNodeName: string): Promise<ArchivistInstance> => {
+  return asArchivistInstance(await getModuleByNameFromChildNode(name, childNodeName), 'Failed to cast archivist')
 }
