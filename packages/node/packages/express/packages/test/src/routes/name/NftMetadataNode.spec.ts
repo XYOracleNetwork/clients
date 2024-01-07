503769aa30d565c57b5c6378662e90c74f6f05af
Joel Carter <JoelBCarter@users.noreply.github.com>
2024-01-06 18:33:44 -0600

    Add E2E tests for index

--- packages/node/packages/express/packages/test/src/routes/name/NftMetadataNode.spec.ts
+++ packages/node/packages/express/packages/test/src/routes/name/NftMetadataNode.spec.ts
@@ -1,11 +1,14 @@
+import { delay } from '@xylabs/delay'
 import { Bridge } from '@xyo-network/bridge-model'
+import { DivinerInstance } from '@xyo-network/diviner-model'
 import { Payload } from '@xyo-network/payload-model'
 import { SentinelInstance } from '@xyo-network/sentinel-model'
 
-import { getBridgeToChildNode, getSentinelByNameFromChildNode } from '../../testUtil'
+import { getBridgeToChildNode, getDivinerByNameFromChildNode, getSentinelByNameFromChildNode } from '../../testUtil'
 
 const nodeName = 'NftMetadataNode'
 const sentinelName = 'NftMetadataSentinel'
+const indexDivinerName = 'NftMetadataIndexDiviner'
 
 describe(`${nodeName}`, () => {
   let bridge: Bridge
@@ -29,6 +32,23 @@ describe(`${nodeName}`, () => {
       expect(result).toBeArray()
       const response = result.filter((p) => p.schema === 'network.xyo.api.call.result')
       expect(response).toBeArrayOfSize(1)
+    })
+  })
+  describe(`${indexDivinerName}`, () => {
+    let diviner: DivinerInstance
+    beforeAll(async () => {
+      await delay(300) // Allow enough time for index to propagate
+      diviner = await getDivinerByNameFromChildNode(indexDivinerName, nodeName)
+      expect(diviner).toBeDefined()
+    })
+    it('queries NFT metadata URI', async () => {
+      const query = {
+        schema: 'network.xyo.diviner.payload.query',
+        uri: 'https://gutter-cats-metadata.s3.us-east-2.amazonaws.com/metadata/1347',
+      }
+      const result = (await diviner.divine([query])) as unknown as Payload<{ address?: string; chainId?: number; tokenId?: string }>[]
+      expect(result).toBeDefined()
+      expect(result).toBeArrayOfSize(1)
     })
   })
 })
--- packages/node/packages/express/packages/test/src/testUtil/Diviner/getDiviner.ts
+++ packages/node/packages/express/packages/test/src/testUtil/Diviner/getDiviner.ts
@@ -2,9 +2,13 @@ import { AccountInstance } from '@xyo-network/account-model'
 import { asDivinerInstance, DivinerInstance } from '@xyo-network/diviner-model'
 import { DivinerWrapper } from '@xyo-network/diviner-wrapper'
 
-import { getModuleByName } from '../Node'
+import { getModuleByName, getModuleByNameFromChildNode } from '../Node'
 
 export const getDivinerByName = async (name: string, account?: AccountInstance): Promise<DivinerInstance> => {
   const diviner = asDivinerInstance(await getModuleByName(name), 'Failed to cast diviner')
   return account ? DivinerWrapper.wrap(diviner, account) : diviner
+}
+
+export const getDivinerByNameFromChildNode = async (name: string, childNodeName: string): Promise<DivinerInstance> => {
+  return asDivinerInstance(await getModuleByNameFromChildNode(name, childNodeName), 'Failed to cast diviner')
 }
