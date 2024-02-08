a6a453c102f7af3d0352353830c708524362f4d6
Joel Carter <JoelBCarter@users.noreply.github.com>
2024-02-08 08:59:09 -0600

    Add new helpers

---
+++ packages/modules-mongo/packages/payload/src/Mappings/fromDbRepresentation.ts
@@ -0,0 +1,35 @@
+import { BoundWitness, BoundWitnessFields, isBoundWitness } from '@xyo-network/boundwitness-model'
+import { deepOmitPrefixedFields } from '@xyo-network/hash'
+import { Payload, PayloadMetaFields } from '@xyo-network/payload-model'
+
+import { BoundWitnessWithMongoMeta } from '../BoundWitness'
+import { PayloadWithMongoMeta } from '../Payload'
+
+export const toReturnValue = <
+  T = PayloadWithMongoMeta | BoundWitnessWithMongoMeta,
+  U = T extends PayloadWithMongoMeta ? Payload<PayloadMetaFields> : BoundWitness,
+>(
+  value: T,
+): U => {
+  const { _$hash, _$meta, ...other } = value as PayloadWithMongoMeta
+  const sanitized = deepOmitPrefixedFields(other, '_')
+  return { $hash: _$hash, $meta: _$meta, ...sanitized } as U
+}
+
+export const payloadFromDbRepresentation = (value: PayloadWithMongoMeta): Payload<PayloadMetaFields> => {
+  const { _$hash, _$meta, ...other } = value
+  const sanitized = deepOmitPrefixedFields(other, '_')
+  return { ...sanitized, $hash: _$hash, $meta: _$meta }
+}
+
+export const boundWitnessFromDbRepresentation = (value: BoundWitnessWithMongoMeta): BoundWitness<BoundWitnessFields> => {
+  const { _$hash, _$meta, ...other } = value
+  const sanitized = deepOmitPrefixedFields(other, '_')
+  return { ...sanitized, $hash: _$hash, $meta: _$meta } as unknown as BoundWitness<BoundWitnessFields>
+}
+
+export const fromDbRepresentation = <T = PayloadWithMongoMeta | BoundWitnessWithMongoMeta>(value: T) => {
+  return isBoundWitness(value)
+    ? (boundWitnessFromDbRepresentation(value as unknown as BoundWitnessWithMongoMeta) as BoundWitness<BoundWitnessFields>)
+    : (payloadFromDbRepresentation(value as PayloadWithMongoMeta) as Payload<PayloadMetaFields>)
+}
--- packages/modules-mongo/packages/payload/src/Mappings/toPayloadWithMongoMeta.ts
+++ packages/modules-mongo/packages/payload/src/Mappings/toPayloadWithMongoMeta.ts
@@ -1,6 +1,8 @@
+import { BoundWitness } from '@xyo-network/boundwitness-model'
 import { PayloadBuilder } from '@xyo-network/payload-builder'
 import { Payload } from '@xyo-network/payload-model'
 
+import { BoundWitnessMongoMeta } from '../BoundWitness'
 import { PayloadWithMongoMeta } from '../Payload'
 
 export const toPayloadWithMongoMeta = async <T extends Payload>(payload: T): Promise<PayloadWithMongoMeta<T>> => {
@@ -8,4 +10,18 @@ export const toPayloadWithMongoMeta = async <T extends Payload>(payload: T): Pro
   const _hash = await PayloadBuilder.hash(built)
   const { $hash, $meta, ...fields } = built
   return { ...fields, _$hash: $hash, _$meta: $meta, _hash, _timestamp: Date.now() } as PayloadWithMongoMeta<T>
+}
+
+export const payloadToDbRepresentation = async <T extends Payload>(payload: T): Promise<PayloadWithMongoMeta<T>> => {
+  const built = await PayloadBuilder.build(payload)
+  const _hash = await PayloadBuilder.hash(built)
+  const { $hash, $meta, ...fields } = built
+  return { ...fields, _$hash: $hash, _$meta: $meta, _hash, _timestamp: Date.now() } as PayloadWithMongoMeta<T>
+}
+
+export const boundWitnessToDbRepresentation = async <T extends BoundWitness>(payload: T): Promise<BoundWitnessMongoMeta<T>> => {
+  const built = await PayloadBuilder.build(payload)
+  const _hash = await PayloadBuilder.hash(built)
+  const { $hash, $meta, ...fields } = built
+  return { ...fields, _$hash: $hash, _$meta: $meta, _hash, _timestamp: Date.now() } as unknown as BoundWitnessMongoMeta<T>
 }
