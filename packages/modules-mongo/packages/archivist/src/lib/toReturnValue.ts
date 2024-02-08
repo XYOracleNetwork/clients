41636b851d21551a5c9e2d8b798a4b4938d2bae5
Joel Carter <JoelBCarter@users.noreply.github.com>
2024-02-08 08:16:00 -0600

    Generics on toReturnValue

--- packages/modules-mongo/packages/archivist/src/lib/toReturnValue.ts
+++ packages/modules-mongo/packages/archivist/src/lib/toReturnValue.ts
@@ -1,9 +1,15 @@
+import { BoundWitness } from '@xyo-network/boundwitness-model'
 import { deepOmitPrefixedFields } from '@xyo-network/hash'
 import { Payload, PayloadMetaFields } from '@xyo-network/payload-model'
 import { BoundWitnessWithMongoMeta, PayloadWithMongoMeta } from '@xyo-network/payload-mongodb'
 
-export const toReturnValue = (value: PayloadWithMongoMeta | BoundWitnessWithMongoMeta): Payload<PayloadMetaFields> => {
-  const { _$hash, _$meta, ...other } = value
+export const toReturnValue = <
+  T = PayloadWithMongoMeta | BoundWitnessWithMongoMeta,
+  U = T extends PayloadWithMongoMeta ? Payload<PayloadMetaFields> : BoundWitness,
+>(
+  value: T,
+): U => {
+  const { _$hash, _$meta, ...other } = value as PayloadWithMongoMeta
   const sanitized = deepOmitPrefixedFields(other, '_')
-  return { $hash: _$hash, $meta: _$meta, ...sanitized } as Payload<PayloadMetaFields>
+  return { $hash: _$hash, $meta: _$meta, ...sanitized } as U
 }
