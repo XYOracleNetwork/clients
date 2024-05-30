8f80363749cad7707a40736cf01652de23e21e80
Joel Carter <JoelBCarter@users.noreply.github.com>
2024-05-30 12:02:48 -0500

    use wallet for node

--- packages/automation-witness-server/src/Jobs/TZeroStockMarket/getTask.ts
+++ packages/automation-witness-server/src/Jobs/TZeroStockMarket/getTask.ts
@@ -1,24 +1,23 @@
 import { assertEx } from '@xylabs/assert'
 import { getDefaultLogger } from '@xylabs/sdk-api-express-ecs'
-import { HDWallet } from '@xyo-network/account'
 import { ApiCallWitness } from '@xyo-network/api-call-witness'
 import { ManifestWrapper, PackageManifestPayload } from '@xyo-network/manifest'
 import { ModuleFactoryLocator } from '@xyo-network/module-factory-locator'
 import { ModuleFactory } from '@xyo-network/module-model'
 import { asSentinelInstance } from '@xyo-network/sentinel-model'
-import { Job } from '@xyo-network/shared'
 import { TZeroApiCallJsonResultToSnapshotDiviner } from '@xyo-network/tzero-stock-market-plugin'
 
+import { getWallet } from '../../Account'
 import tZeroMarketSnapshotDiviner from './ApiCallWitnessManifest.json'
 
-export const getTask = (): Job['task'] => {
+export const getTask = () => {
   const logger = getDefaultLogger()
   const apiKey = assertEx(process.env.TZERO_MARKETDATA_API_KEY, () => 'TZERO_MARKETDATA_API_KEY is not set')
-  const task: Job['task'] = async () => {
+  const task = async () => {
     try {
       logger.log('Reporting TZero Stock Prices')
       await Promise.resolve()
-      const wallet = await HDWallet.random()
+      const wallet = await getWallet()
       const locator = new ModuleFactoryLocator()
       locator.register(new ModuleFactory(ApiCallWitness, { headers: { 'x-apikey': apiKey } }))
       locator.register(TZeroApiCallJsonResultToSnapshotDiviner)
