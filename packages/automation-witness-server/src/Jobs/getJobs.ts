410950d6364d6e62e1ff3ba6c7bc698456f8cf98
Joel Carter <JoelBCarter@users.noreply.github.com>
2024-05-30 11:34:10 -0500

    Rename dir

--- packages/automation-witness-server/src/Jobs/getJobs.ts
+++ packages/automation-witness-server/src/Jobs/getJobs.ts
@@ -2,7 +2,7 @@ import { Job } from '@xyo-network/shared'
 
 import { getJob as getCryptoMarketWitnessJob } from './CryptoMarket'
 import { getJob as getEthereumGasJob } from './EthereumGas'
-import { getJob as getTZeroStockPriceJob } from './TZero'
+import { getJob as getTZeroStockPriceJob } from './TZeroStockMarket'
 
 export const getJobs = (): Job[] => {
   return [getCryptoMarketWitnessJob(), getEthereumGasJob(), getTZeroStockPriceJob()]
