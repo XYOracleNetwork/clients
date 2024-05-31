192007f82cd6fdde8efd2f0ceabddd586c4b3ef7
Joel Carter <JoelBCarter@users.noreply.github.com>
2024-05-31 17:16:16 -0500

    Add identity helpers

--- packages/automation-witness-server/src/Jobs/TZeroStockMarket/spec/reportStockPrices.spec.ts
+++ packages/automation-witness-server/src/Jobs/TZeroStockMarket/spec/reportStockPrices.spec.ts
@@ -1,11 +1,24 @@
+import { assertEx } from '@xylabs/assert'
+import { isSnapshotWithMeta } from '@xyo-network/tzero-stock-market-payload-plugin'
+
+import { getArchivist } from '../../../Archivists'
 import { reportStockPrice } from '../reportStockPrices'
 
 describe('reportStockPrices', () => {
   describe('reportStockPrice', () => {
     it('reports stock price', async () => {
       const result = await reportStockPrice('XYLB')
-      expect(result).toBeArray()
-      expect(result.length).toBeGreaterThan(0)
+      expect(result).toBeArrayOfSize(2)
+      const snapshot = result.find(isSnapshotWithMeta)
+      expect(snapshot).toBeDefined()
+      expect(snapshot?.symbol).toBe('XYLB')
+      expect(snapshot?.$hash).toBeDefined()
+      const hash = assertEx(snapshot?.$hash, () => 'Missing hash')
+      const archivist = await getArchivist()
+      expect(archivist).toBeDefined()
+      const stored = await archivist.get([hash])
+      expect(stored).toBeArray()
+      expect(stored.length).toBeGreaterThan(0)
     })
   })
 })
