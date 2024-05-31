6bf9e56dfc9161b6f7e6470bf0c4498581f7792f
Joel Carter <JoelBCarter@users.noreply.github.com>
2024-05-31 17:27:17 -0500

    Add notes on implementation

--- packages/automation-witness-server/src/Jobs/TZeroStockMarket/getNode.ts
+++ packages/automation-witness-server/src/Jobs/TZeroStockMarket/getNode.ts
@@ -18,6 +18,10 @@ export const getNode = async (): Promise<MemoryNode> => {
   locator.register(TZeroApiCallJsonResultToSnapshotDiviner)
   const manifest = new ManifestWrapper(tZeroMarketSnapshotDiviner as PackageManifestPayload, wallet, locator)
   const node = await manifest.loadNodeFromIndex(0)
+  // Attach archivist to node to allow for dynamic archiving of
+  // sentinel reports based on environment. The Archivist is
+  // already declared in the manifest but doesn't exist as we
+  // dynamically bridge to it based on the environment.
   const archivist = await getArchivist()
   await node.register(archivist)
   await node.attach(archivist.address, true)
