94a012c83a4a5721f9635b35bf380280dbea5cac
Joel Carter <JoelBCarter@users.noreply.github.com>
2024-05-31 16:52:25 -0500

    Add helper for testability of task

--- packages/automation-witness-server/src/Jobs/TZeroStockMarket/getTask.ts
+++ packages/automation-witness-server/src/Jobs/TZeroStockMarket/getTask.ts
@@ -1,20 +1,13 @@
-import { assertEx } from '@xylabs/assert'
 import { getDefaultLogger } from '@xylabs/sdk-api-express-ecs'
-import { ApiCallSchema, ApiUriTemplateCall } from '@xyo-network/api-call-witness'
-import { asSentinelInstance } from '@xyo-network/sentinel-model'
 
-import { getNode } from './getNode'
+import { reportStockPrice } from './reportStockPrices'
 
 export const getTask = () => {
   const logger = getDefaultLogger()
-  const call: ApiUriTemplateCall = { params: { symbol: 'XYLB' }, schema: ApiCallSchema }
   const task = async () => {
     try {
       logger.log('Reporting TZero Stock Prices')
-      const node = await getNode()
-      const sentinelInstance = asSentinelInstance(await node.resolve('ApiCallSentinel'))
-      const sentinel = assertEx(sentinelInstance, () => 'ApiCallSentinel not found')
-      await sentinel.report([call])
+      await reportStockPrice('XYLB')
       logger.log('Reported TZero Stock Prices')
     } catch (error) {
       logger.error(error)
--- packages/automation-witness-server/src/Jobs/TZeroStockMarket/reportStockPrices.ts
+++ packages/automation-witness-server/src/Jobs/TZeroStockMarket/reportStockPrices.ts
@@ -1,35 +1,18 @@
-import { AttachableModuleInstance } from '@xyo-network/module-model'
-import { MemoryNode } from '@xyo-network/node-memory'
+import { assertEx } from '@xylabs/assert'
+import { ApiCallSchema, ApiUriTemplateCall } from '@xyo-network/api-call-witness'
 import { Payload } from '@xyo-network/payload-model'
-import { MemorySentinel, SentinelConfig, SentinelConfigSchema } from '@xyo-network/sentinel'
+import { asSentinelInstance } from '@xyo-network/sentinel'
 
-import { getAccount, WalletPaths } from '../../Account'
-import { getArchivists } from '../../Archivists'
+import { getNode } from './getNode'
 
-export const reportStockPrices = async (): Promise<Payload[]> => {
-  const archivists = await getArchivists()
-  const modules: AttachableModuleInstance[] = [...archivists]
-  const node = await MemoryNode.create()
-  await Promise.all(
-    modules.map(async (mod) => {
-      await node.register(mod)
-      await node.attach(mod.address, true)
-    }),
-  )
-  const config: SentinelConfig = {
-    archiving: {
-      archivists: archivists.map((mod) => mod.address),
-    },
-    schema: SentinelConfigSchema,
-    synchronous: true,
-    // TODO: How to create new sentinel or add archivists to existing sentinel from manifest
-    // tasks: witnesses.map((mod) => ({ module: mod.address })),
-    tasks: [],
-  }
-  const account = await getAccount(WalletPaths.CryptoMarket.Sentinel.Market)
-  const sentinel = await MemorySentinel.create({ account, config })
-  await node.register(sentinel)
-  await node.attach(account.address, true)
-  const report = await sentinel.report()
-  return report
+export const reportStockPrice = async (symbol: string): Promise<Payload[]> => {
+  const call: ApiUriTemplateCall = { params: { symbol }, schema: ApiCallSchema }
+  const node = await getNode()
+  const sentinelInstance = asSentinelInstance(await node.resolve('ApiCallSentinel'))
+  const sentinel = assertEx(sentinelInstance, () => 'ApiCallSentinel not found')
+  return await sentinel.report([call])
+}
+
+export const reportStockPrices = async (symbols: string[] = ['XYLB']): Promise<Payload[]> => {
+  return (await Promise.all(symbols.map((symbol) => reportStockPrice(symbol)))).flat()
 }
