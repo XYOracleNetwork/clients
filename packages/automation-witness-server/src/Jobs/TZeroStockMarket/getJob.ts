0812d955eb1cdf6227435195350c0484223d729f
Joel Carter <JoelBCarter@users.noreply.github.com>
2024-05-31 17:31:15 -0500

    Prefix ENV VAR to group with similar

--- packages/automation-witness-server/src/Jobs/TZeroStockMarket/getJob.ts
+++ packages/automation-witness-server/src/Jobs/TZeroStockMarket/getJob.ts
@@ -5,7 +5,7 @@ import { getTask } from './getTask'
 const name = 'tZeroStockPriceWitness'
 
 export const getJob = (): Job => {
-  const schedule: string = process.env.STOCK_MARKET_WITNESS_JOB_SCHEDULE || '10 minutes'
+  const schedule: string = process.env.TZERO_STOCK_MARKET_WITNESS_JOB_SCHEDULE || '10 minutes'
   const task = getTask()
   return { name, schedule, task }
 }
