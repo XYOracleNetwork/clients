497b79e8d8ccb0130a18134526c4760db5d310cc
Joel Carter <JoelBCarter@users.noreply.github.com>
2024-05-31 12:30:52 -0500

    Archivist helpers

--- packages/automation-witness-server/src/Archivists/getArchivists.ts
+++ packages/automation-witness-server/src/Archivists/getArchivists.ts
@@ -1,6 +1,7 @@
+import { assertEx } from '@xylabs/assert'
 import { HDWallet } from '@xyo-network/account'
 import { ApiConfig } from '@xyo-network/api-models'
-import { asAttachableArchivistInstance, AttachableArchivistInstance, isArchivistModule } from '@xyo-network/archivist-model'
+import { AttachableArchivistInstance, isAttachableArchivistInstance } from '@xyo-network/archivist-model'
 import { HttpBridge, HttpBridgeConfigSchema } from '@xyo-network/bridge-http'
 
 import { getApiConfig } from './getApiConfig'
@@ -8,17 +9,23 @@ import { getApiConfig } from './getApiConfig'
 const schema = HttpBridgeConfigSchema
 const security = { allowAnonymous: true }
 
+export const getArchivist = async (config: ApiConfig = getApiConfig()): Promise<AttachableArchivistInstance> => {
+  return assertEx(await tryGetArchivist(config), () => 'Archivist not found')
+}
+
+export const tryGetArchivist = async (config: ApiConfig = getApiConfig()): Promise<AttachableArchivistInstance | undefined> => {
+  const nodeUrl = `${config.apiDomain}/node`
+  const bridge = await HttpBridge.create({ account: await HDWallet.random(), config: { nodeUrl, schema, security } })
+  await bridge.start()
+  const module = await bridge.resolve('XYOPublic:Archivist')
+  return isAttachableArchivistInstance(module) ? module : undefined
+}
+
 export const getArchivists = async (configs: ApiConfig[] = [getApiConfig()]): Promise<AttachableArchivistInstance[]> => {
   const archivists: AttachableArchivistInstance[] = []
   for (const config of configs) {
-    const nodeUrl = `${config.apiDomain}/node`
-    const bridge = await HttpBridge.create({ account: await HDWallet.random(), config: { nodeUrl, schema, security } })
-    await bridge.start()
-    const module = await bridge.resolve('XYOPublic:Archivist')
-    const mod = asAttachableArchivistInstance(module, 'Error resolving Archivist')
-    if (isArchivistModule(mod)) {
-      archivists.push(mod)
-    }
+    const archivist = await tryGetArchivist(config)
+    if (archivist) archivists.push(archivist)
   }
   return archivists
 }
