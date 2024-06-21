f234fe3c5aaa76a444d1e5729357b62f169d81df
Joel Carter <JoelBCarter@users.noreply.github.com>
2024-06-21 12:57:49 -0500

    Run every 6 hours

--- packages/automation-witness-server/src/Jobs/XyoMediumRss/getJob.ts
+++ packages/automation-witness-server/src/Jobs/XyoMediumRss/getJob.ts
@@ -5,7 +5,7 @@ import { getTask } from './getTask'
 const name = 'xyoMediumRss'
 
 export const getJob = (): Job => {
-  const schedule: string = process.env.XYO_MEDIUM_RSS_WITNESS_SCHEDULE || '4 hours'
+  const schedule: string = process.env.XYO_MEDIUM_RSS_WITNESS_SCHEDULE || '6 hours'
   const task = getTask()
   return { name, schedule, task }
 }
