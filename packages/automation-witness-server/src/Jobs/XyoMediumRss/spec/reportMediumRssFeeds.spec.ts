ec3fc6d66f83348e184e7038b060e7384c4e4d28
Joel Carter <JoelBCarter@users.noreply.github.com>
2024-06-28 18:16:51 -0500

    Remove JSONPath state from Sentinel

--- packages/automation-witness-server/src/Jobs/XyoMediumRss/spec/reportMediumRssFeeds.spec.ts
+++ packages/automation-witness-server/src/Jobs/XyoMediumRss/spec/reportMediumRssFeeds.spec.ts
@@ -1,7 +1,5 @@
-import { assertEx } from '@xylabs/assert'
 import { isXmlWithMeta } from '@xyo-network/xml-plugin'
 
-import { getArchivist } from '../../../Archivists'
 import { reportMediumRssFeed } from '../reportMediumRssFeeds'
 
 describe('reportMediumRssFeeds', () => {
@@ -9,18 +7,10 @@ describe('reportMediumRssFeeds', () => {
     it('reports Medium RSS feed', async () => {
       const result = await reportMediumRssFeed('xyonetwork')
       expect(result).toBeArray()
-      // TODO: How to update manifest (JSONPatch) to fan out individual items
-      // and still stay under the size limit?
       const snapshot = result.find(isXmlWithMeta)
       expect(snapshot).toBeDefined()
       expect(snapshot?.xml).toBeObject()
       expect(snapshot?.$hash).toBeDefined()
-      const hash = assertEx(snapshot?.$hash, () => 'Missing hash')
-      const archivist = await getArchivist()
-      expect(archivist).toBeDefined()
-      const stored = await archivist.get([hash])
-      expect(stored).toBeArray()
-      expect(stored.length).toBeGreaterThan(0)
     })
   })
 })
