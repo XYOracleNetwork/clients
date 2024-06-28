558b3e5a84d14268cd0ad18c7c489cbc0d8c0d6d
Joel Carter <JoelBCarter@users.noreply.github.com>
2024-06-28 18:48:06 -0500

    Task loop flow for witnessing unique articles

--- packages/automation-witness-server/src/Jobs/XyoMediumRss/getTask.ts
+++ packages/automation-witness-server/src/Jobs/XyoMediumRss/getTask.ts
@@ -3,6 +3,7 @@ import { getDefaultLogger } from '@xylabs/sdk-api-express-ecs'
 import { reportMediumRssFeed } from './reportMediumRssFeeds'
 import { getArchivist } from '../../Archivists'
 import { Payload } from '@xyo-network/payload-model'
+import { PayloadBuilder } from '@xyo-network/payload-builder'
 
 export const getTask = () => {
   const logger = getDefaultLogger()
@@ -12,11 +13,21 @@ export const getTask = () => {
       const rss = await reportMediumRssFeed('xyonetwork')
       // TODO: Divine individual articles from the RSS feed
       const articles: Payload[] = []
+      const uniqueArticles: Payload[] = []
+      const archivist = await getArchivist()
+      // Check for each article if it has already been reported
+      for (const article of articles) {
+        const hash = await PayloadBuilder.dataHash(article)
+        const existing = await archivist.get([hash])
+        if (existing.length === 0) {
+          uniqueArticles.push(article)
+        }
+      }
       // Save individual articles to the archivist, one at
       // a time because of size constraints on the payload
-      const archivist = await getArchivist()
-      for (const article of articles) {
-        await archivist.insert([article])
+      for (const article of uniqueArticles) {
+        // TODO: Sentinel report the article
+        // to ensure consistent address
       }
       logger.log('Reported XYO Medium RSS Feed')
     } catch (error) {
