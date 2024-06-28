f72ff47d88a7cbcf05f85e05bacf09c092b68949
Joel Carter <JoelBCarter@users.noreply.github.com>
2024-06-28 18:29:25 -0500

    Individually insert each article from the RSS feed

--- packages/automation-witness-server/src/Jobs/XyoMediumRss/getTask.ts
+++ packages/automation-witness-server/src/Jobs/XyoMediumRss/getTask.ts
@@ -1,13 +1,23 @@
 import { getDefaultLogger } from '@xylabs/sdk-api-express-ecs'
 
 import { reportMediumRssFeed } from './reportMediumRssFeeds'
+import { getArchivist } from '../../Archivists'
+import { Payload } from '@xyo-network/payload-model'
 
 export const getTask = () => {
   const logger = getDefaultLogger()
   const task = async () => {
     try {
       logger.log('Reporting XYO Medium RSS Feed')
-      await reportMediumRssFeed('xyonetwork')
+      const rss = await reportMediumRssFeed('xyonetwork')
+      // TODO: Divine individual articles from the RSS feed
+      const articles: Payload[] = []
+      // Save individual articles to the archivist, one at
+      // a time because of size constraints on the payload
+      const archivist = await getArchivist()
+      for (const article of articles) {
+        await archivist.insert([article])
+      }
       logger.log('Reported XYO Medium RSS Feed')
     } catch (error) {
       logger.error(error)
