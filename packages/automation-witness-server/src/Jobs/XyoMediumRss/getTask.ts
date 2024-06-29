dfe993190d53d1f0f527845ae16ebcdd18c6b7b4
Joel Carter <JoelBCarter@users.noreply.github.com>
2024-06-28 19:05:34 -0500

    Add unique article report to task

--- packages/automation-witness-server/src/Jobs/XyoMediumRss/getTask.ts
+++ packages/automation-witness-server/src/Jobs/XyoMediumRss/getTask.ts
@@ -1,9 +1,7 @@
 import { getDefaultLogger } from '@xylabs/sdk-api-express-ecs'
 
 import { reportMediumRssFeed } from './reportMediumRssFeeds'
-import { getArchivist } from '../../Archivists'
-import { Payload } from '@xyo-network/payload-model'
-import { PayloadBuilder } from '@xyo-network/payload-builder'
+import { reportMediumBlogPost } from './reportMediumBlogPost'
 
 export const getTask = () => {
   const logger = getDefaultLogger()
@@ -11,25 +9,10 @@ export const getTask = () => {
     try {
       logger.log('Reporting XYO Medium RSS Feed')
       const rss = await reportMediumRssFeed('xyonetwork')
-      // TODO: Divine individual articles from the RSS feed
-      const articles: Payload[] = []
-      const uniqueArticles: Payload[] = []
-      const archivist = await getArchivist()
-      // Check for each article if it has already been reported
-      for (const article of articles) {
-        const hash = await PayloadBuilder.dataHash(article)
-        const existing = await archivist.get([hash])
-        if (existing.length === 0) {
-          uniqueArticles.push(article)
-        }
-      }
-      // Save individual articles to the archivist, one at
-      // a time because of size constraints on the payload
-      for (const article of uniqueArticles) {
-        // TODO: Sentinel report the article
-        // to ensure consistent address
-      }
       logger.log('Reported XYO Medium RSS Feed')
+      logger.log('Reporting XYO Medium Articles')
+      await reportMediumBlogPost(rss)
+      logger.log('Reported XYO Medium Articles')
     } catch (error) {
       logger.error(error)
     }
