acdea10cc2d5a16f3be83a59afad1f43080b4347
Joel Carter <JoelBCarter@users.noreply.github.com>
2024-07-16 14:14:23 -0500

    Remove unused eslint exclusions

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -44,10 +44,6 @@ const finalConfig = {
     'import/no-internal-modules': [
       'warn', {
         'allow': [
-          'source-map-support/*',
-          'lodash/*',
-          'aws-sdk/**/*',
-          'types/*',
           '**/index.js', // Allow imports to any index.js file
           '**/index.ts',
           '**/index.jsx',
