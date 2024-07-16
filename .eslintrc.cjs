07c409af6e0bd1c17259e588fb0ba87610041c31
Joel Carter <JoelBCarter@users.noreply.github.com>
2024-07-16 14:43:27 -0500

    Use import/no-internal-modules from base config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,31 +3,8 @@ const finalConfig = {
   parser: '@typescript-eslint/parser',
   parserOptions: { ecmaVersion: 'latest', project: './tsconfig-eslint.json', sourceType: 'module', tsconfigRootDir: null },
   'root': true,
-  'ignorePatterns': [
-    'dist',
-    'node_modules',
-    '**/node_modules',
-    '**/dist',
-    'docs',
-    'build',
-    'coverage',
-    'public',
-    'scripts',
-    '.yarn',
-    '.*'
-  ],
   'rules': {
-    'id-denylist': ['warn', 'module'],
-    'import/no-internal-modules': [
-      'warn', {
-        'allow': [
-          '**/index.js', // Allow imports to any index.js file
-          '**/index.ts',
-          '**/index.jsx',
-          '**/index.tsx'
-        ]
-      }
-    ]
+    'id-denylist': ['warn', 'module']
   }
 }
 
