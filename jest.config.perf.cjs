aba513956b9e3c254f5d703e5c86b76e78da369c
Joel Carter <JoelBCarter@users.noreply.github.com>
2024-07-17 07:12:41 -0500

    Update additional jest configs

--- jest.config.perf.cjs
+++ jest.config.perf.cjs
@@ -2,10 +2,13 @@ const generateJestConfig = ({ esModules }) => {
   const esModulesList = Array.isArray(esModules) ? esModules.join('|') : esModules
   return {
     coveragePathIgnorePatterns: ['<rootDir>/(.*)/dist'],
+    extensionsToTreatAsEsm: ['.ts'],
+    maxWorkers: '100%',
     moduleNameMapper: {
       '^(\\.{1,2}/.*)\\.js$': '$1',
     },
-    preset: 'ts-jest/presets/default-esm',
+    preset: 'ts-jest',
+    runner: 'groups',
     setupFiles: ['dotenv/config'],
     setupFilesAfterEnv: ['jest-sorted', 'jest-extended/all', './packages/node/packages/express/packages/test/src/setupFiles.ts'],
     testRegex: String.raw`(/__tests__/.*|(\.|/)perf\.(test|spec))\.tsx?$`,
@@ -16,12 +19,13 @@ const generateJestConfig = ({ esModules }) => {
         'ts-jest',
         {
           tsconfig: 'tsconfig.test.json',
+          useESM: true,
         },
       ],
     },
     transformIgnorePatterns: [`./node_modules/(?!${esModulesList})`],
+    workerThreads: true,
   }
 }
 
- 
 module.exports = generateJestConfig({ esModules: ['is-ip', 'ip-regex', 'lodash-es', 'uuid'] })
