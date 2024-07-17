0d27681e84d04d468a9648f23c0e4631955514eb
Joel Carter <JoelBCarter@users.noreply.github.com>
2024-07-17 07:15:38 -0500

    Remove medium blog post witness

--- packages/automation-witness-server/src/Jobs/XyoMediumRss/spec/getTask.spec.ts
+++
@@ -1,8 +0,0 @@
-import { getTask } from '../getTask.js'
-
-describe('getTask', () => {
-  it('gets the job', () => {
-    const task = getTask()
-    expect(task).toBeFunction()
-  })
-})
--- packages/automation-witness-server/src/Jobs/getJobs.ts
+++ packages/automation-witness-server/src/Jobs/getJobs.ts
@@ -3,8 +3,7 @@ import { Job } from '@xyo-network/shared'
 import { getJob as getCryptoMarketWitnessJob } from './CryptoMarket/index.js'
 import { getJob as getEthereumGasJob } from './EthereumGas/index.js'
 import { getJob as getTZeroStockPriceJob } from './TZeroStockMarket/index.js'
-import { getJob as getMediumRssJob } from './XyoMediumRss/index.js'
 
 export const getJobs = (): Job[] => {
-  return [getCryptoMarketWitnessJob(), getEthereumGasJob(), getTZeroStockPriceJob(), getMediumRssJob()]
+  return [getCryptoMarketWitnessJob(), getEthereumGasJob(), getTZeroStockPriceJob()]
 }
