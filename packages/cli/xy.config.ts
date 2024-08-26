fe93c28266c511e5d83c9d38131b95b14c02cc0e
Arie Trouw <arie.trouw@xylabs.com>
2024-08-26 09:51:27 -0700

    esm only version

--- packages/cli/xy.config.ts
+++ packages/cli/xy.config.ts
@@ -1,10 +1,11 @@
-import { XyTsupConfig } from '@xylabs/ts-scripts-yarn3'
+import type { XyTsupConfig } from '@xylabs/ts-scripts-yarn3'
 const config: XyTsupConfig = {
   compile: {
     browser: {},
+    neutral: {},
     entryMode: 'all',
     node: {
-      src: true,
+      src: {entry: ["src/**/*.ts", "!src/**/*.spec.*"]},
     },
   },
 }
