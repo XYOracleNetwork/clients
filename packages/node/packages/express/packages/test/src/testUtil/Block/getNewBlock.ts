3c016ef8eb7edc2a9abbeff1a399f24b01a7808d
Joel Carter <JoelBCarter@users.noreply.github.com>
2024-12-10 16:54:49 -0600

    Re-utilize BW Builder type and helper methods

--- packages/node/packages/express/packages/test/src/testUtil/Block/getNewBlock.ts
+++ packages/node/packages/express/packages/test/src/testUtil/Block/getNewBlock.ts
@@ -2,18 +2,14 @@ import { BoundWitnessBuilder } from '@xyo-network/boundwitness-builder'
 import type { BoundWitness } from '@xyo-network/boundwitness-model'
 import type { Payload } from '@xyo-network/payload-model'
 
-import { unitTestSigningAccount } from '../Account/index.js'
-import { getNewPayloads } from '../Payload/index.js'
+import { unitTestSigningAccount } from '../Account/index.ts'
+import { getNewBoundWitnesses } from '../BoundWitness/index.ts'
 
-export const getNewBlock = async (...payloads: Payload[]): Promise<[BoundWitness, Payload[]]> => {
-  const [bw, p] = await new BoundWitnessBuilder().signer(await unitTestSigningAccount()).payloads(payloads).build()
-  return [bw, p]
-}
+type BoundWitnessBuilderBuildResult = Awaited<ReturnType<BoundWitnessBuilder['build']>>
 
-export const getNewBlockWithPayloads = async (numPayloads = 1): Promise<[BoundWitness, Payload[]]> => {
-  const blocksWithPayloads = await getNewBlocksWithPayloads(numPayloads)
-  const [bw, payloads] = blocksWithPayloads[0]
-  return [bw, payloads]
+export const getNewBlock = async (...payloads: Payload[]): Promise<BoundWitnessBuilderBuildResult> => {
+  const signer = await unitTestSigningAccount()
+  return await new BoundWitnessBuilder().signer(signer).payloads(payloads).build()
 }
 
 export const getNewBlocks = async (numBoundWitnesses = 1): Promise<BoundWitness[]> => {
@@ -24,12 +20,6 @@ export const getNewBlocks = async (numBoundWitnesses = 1): Promise<BoundWitness[
 export const getNewBlocksWithPayloads = async (
   numBoundWitnesses = 1,
   numPayloads = 1,
-): Promise<[BoundWitness, Payload[]][]> => {
-  return await Promise.all(
-    Array.from({ length: numBoundWitnesses })
-      .map(async () => {
-        const result = await (new BoundWitnessBuilder().signer(await unitTestSigningAccount()).payloads(await getNewPayloads(numPayloads))).build()
-        return [result[0], result[1]]
-      }),
-  )
+): Promise<BoundWitnessBuilderBuildResult[]> => {
+  return await getNewBoundWitnesses([await unitTestSigningAccount()], numBoundWitnesses, numPayloads)
 }
