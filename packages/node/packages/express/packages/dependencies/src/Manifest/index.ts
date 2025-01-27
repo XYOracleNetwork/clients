18ab3703e4741cb75c54c59ef5bc95dc31ab938b
Joel Carter <JoelBCarter@users.noreply.github.com>
2025-01-27 08:25:40 -0600

    Add public children

--- packages/node/packages/express/packages/dependencies/src/Manifest/index.ts
+++ packages/node/packages/express/packages/dependencies/src/Manifest/index.ts
@@ -1,8 +1,13 @@
-import type { PackageManifestPayload } from '@xyo-network/manifest-model'
+import type { ModuleManifest, PackageManifestPayload } from '@xyo-network/manifest-model'
 
+import chain from './chain.json' assert { type: 'json' }
 import node from './node.json' assert { type: 'json' }
 
 /**
  * The default node
  */
 export const defaultNode = node as PackageManifestPayload
+
+// Public children
+const chainManifest = chain as PackageManifestPayload
+export const manifestPublicChildren = [...chainManifest.nodes] as ModuleManifest[]
