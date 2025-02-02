740aa116c01a609bdf9361cbd341618df26e9506
Joel Carter <JoelBCarter@users.noreply.github.com>
2025-02-02 15:54:10 -0600

    Update tests

--- packages/modules-mongo/packages/archivist/src/spec/ArchivistV2.spec.ts
+++ packages/modules-mongo/packages/archivist/src/spec/ArchivistV2.spec.ts
@@ -5,11 +5,10 @@ import { Account } from '@xyo-network/account'
 import type { ArchivistNextOptions } from '@xyo-network/archivist-model'
 import { ArchivistWrapper } from '@xyo-network/archivist-wrapper'
 import { BoundWitnessBuilder } from '@xyo-network/boundwitness-builder'
-import { BoundWitnessWrapper } from '@xyo-network/boundwitness-wrapper'
+import type { BoundWitness } from '@xyo-network/boundwitness-model'
 import { COLLECTIONS, hasMongoDBConfig } from '@xyo-network/module-abstract-mongodb'
 import { PayloadBuilder } from '@xyo-network/payload-builder'
-import type { PayloadWrapperBase } from '@xyo-network/payload-wrapper'
-import { PayloadWrapper } from '@xyo-network/payload-wrapper'
+import type { Payload } from '@xyo-network/payload-model'
 import type { BaseMongoSdkConfig } from '@xyo-network/sdk-xyo-mongo-js'
 import {
   beforeAll,
@@ -24,8 +23,8 @@ describe.runIf(hasMongoDBConfig())('Archivist', () => {
   const boundWitnessesConfig: BaseMongoSdkConfig = { collection: COLLECTIONS.BoundWitnesses }
   const payloadsConfig: BaseMongoSdkConfig = { collection: COLLECTIONS.Payloads }
 
-  const payloadWrappers: PayloadWrapper[] = []
-  const boundWitnessWrappers: BoundWitnessWrapper[] = []
+  const payloads: Payload[] = []
+  const boundWitnesses: BoundWitness[] = []
   let archivist: ArchivistWrapper
   beforeAll(async () => {
     boundWitnessesConfig.dbConnectionString = process.env.MONGO_CONNECTION_STRING
@@ -37,31 +36,24 @@ describe.runIf(hasMongoDBConfig())('Archivist', () => {
       payloadSdkConfig: payloadsConfig,
     })
     archivist = new ArchivistWrapper({ mod: mod, account: await Account.random() })
-    const payload1 = { nonce: Date.now(), schema: 'network.xyo.debug' }
+    const payload1: Payload = new PayloadBuilder({ schema: 'network.xyo.debug' }).fields({ nonce: Date.now() }).build()
     await delay(2)
-    const payload2 = { nonce: Date.now(), schema: 'network.xyo.test' }
+    const payload2: Payload = new PayloadBuilder({ schema: 'network.xyo.test' }).fields({ nonce: Date.now() }).build()
     await delay(2)
-    const payload3 = { nonce: Date.now(), schema: 'network.xyo.debug' }
+    const payload3: Payload = new PayloadBuilder({ schema: 'network.xyo.debug' }).fields({ nonce: Date.now() }).build()
     await delay(2)
-    const payload4 = { nonce: Date.now(), schema: 'network.xyo.test' }
+    const payload4: Payload = new PayloadBuilder({ schema: 'network.xyo.test' }).fields({ nonce: Date.now() }).build()
     await delay(2)
-    const payloadWrapper1 = PayloadWrapper.wrap(payload1)
-    const payloadWrapper2 = PayloadWrapper.wrap(payload2)
-    const payloadWrapper3 = PayloadWrapper.wrap(payload3)
-    const payloadWrapper4 = PayloadWrapper.wrap(payload4)
-    payloadWrappers.push(payloadWrapper1, payloadWrapper2, payloadWrapper3, payloadWrapper4)
+    payloads.push(payload1, payload2, payload3, payload4)
     const signer = await Account.random()
-    const boundWitness1 = (await new BoundWitnessBuilder().payload(payloadWrapper1.payload).signer(signer).build())[0]
+    const boundWitness1 = (await new BoundWitnessBuilder().payload(payload1).signer(signer).build())[0]
     await delay(2)
-    const boundWitness2 = (await new BoundWitnessBuilder().payload(payloadWrapper2.payload).signer(signer).build())[0]
+    const boundWitness2 = (await new BoundWitnessBuilder().payload(payload2).signer(signer).build())[0]
     await delay(2)
     const boundWitness3 = (
-      await new BoundWitnessBuilder().payloads([payloadWrapper3.payload, payloadWrapper4.payload]).signer(signer).build()
+      await new BoundWitnessBuilder().payloads([payload3, payload4]).signer(signer).build()
     )[0]
-    const boundWitnessWrapper1 = BoundWitnessWrapper.parse(boundWitness1, [payload1])
-    const boundWitnessWrapper2 = BoundWitnessWrapper.parse(boundWitness2, [payload2])
-    const boundWitnessWrapper3 = BoundWitnessWrapper.parse(boundWitness3, [payload3, payload4])
-    boundWitnessWrappers.push(boundWitnessWrapper1, boundWitnessWrapper2, boundWitnessWrapper3)
+    boundWitnesses.push(boundWitness1, boundWitness2, boundWitness3)
   })
 
   describe('discover', () => {
@@ -72,69 +64,65 @@ describe.runIf(hasMongoDBConfig())('Archivist', () => {
     })
   })
   describe('insert', () => {
-    const cases: [string, TestDataGetter<PayloadWrapperBase[]>][] = [
-      ['inserts single payload', () => [payloadWrappers[0]]],
-      ['inserts multiple payloads', () => [payloadWrappers[1], payloadWrappers[2]]],
-      ['inserts single boundwitness', () => [boundWitnessWrappers[0]]],
-      ['inserts multiple boundwitness', () => [boundWitnessWrappers[1], boundWitnessWrappers[2]]],
+    const cases: [string, TestDataGetter<Payload[]>][] = [
+      ['inserts single payload', () => [payloads[0]]],
+      ['inserts multiple payloads', () => [payloads[1], payloads[2]]],
+      ['inserts single boundwitness', () => [boundWitnesses[0]]],
+      ['inserts multiple boundwitness', () => [boundWitnesses[1], boundWitnesses[2]]],
     ]
     it.each(cases)('%s', async (_title, getData) => {
       const payloads = getData()
-      const results = await archivist.insert(payloads.map(w => w.payload))
+      const results = await archivist.insert(payloads.map(w => w))
       expect(results).toBeArrayOfSize(payloads.length)
       for (const [i, result] of results.entries()) {
         const payload = payloads[i]
-        expect(await PayloadBuilder.dataHash(result)).toEqual(await payload.dataHash())
-        expect(await PayloadBuilder.dataHash(result)).toEqual(await PayloadBuilder.dataHash(payload.payload))
-        expect(await PayloadBuilder.hash(result)).toEqual(await PayloadBuilder.hash(payload.payload))
-        expect(PayloadBuilder.omitStorageMeta(result)).toEqual(payload.payload)
+        expect(await PayloadBuilder.dataHash(result)).toEqual(await PayloadBuilder.dataHash(payload))
+        expect(await PayloadBuilder.hash(result)).toEqual(await PayloadBuilder.hash(payload))
+        expect(PayloadBuilder.omitStorageMeta(result)).toEqual(payload)
       }
     })
   })
   describe('get', () => {
-    const cases: [string, TestDataGetter<PayloadWrapperBase[]>][] = [
-      ['gets single payload', () => [payloadWrappers[0]]],
-      ['gets multiple payloads', () => [payloadWrappers[1], payloadWrappers[2]]],
-      ['gets single boundwitness', () => [boundWitnessWrappers[0]]],
-      ['gets multiple boundwitness', () => [boundWitnessWrappers[1], boundWitnessWrappers[2]]],
+    const cases: [string, TestDataGetter<Payload[]>][] = [
+      ['gets single payload', () => [payloads[0]]],
+      ['gets multiple payloads', () => [payloads[1], payloads[2]]],
+      ['gets single boundwitness', () => [boundWitnesses[0]]],
+      ['gets multiple boundwitness', () => [boundWitnesses[1], boundWitnesses[2]]],
     ]
     it.each(cases)('%s', async (_title, getData) => {
       const payloads = getData()
-      const results = await archivist.get(await Promise.all(payloads.map(p => p.dataHash())))
+      const hashes = await Promise.all(payloads.map(async payload => await PayloadBuilder.dataHash(payload)))
+      const results = await archivist.get(hashes)
       for (const [i, result] of results.entries()) {
         const payload = payloads[i]
-        expect(await PayloadBuilder.dataHash(result)).toEqual(await payload.dataHash())
-        expect(await PayloadBuilder.dataHash(result)).toEqual(await PayloadBuilder.dataHash(payload.payload))
-        expect(await PayloadBuilder.hash(result)).toEqual(await PayloadBuilder.hash(payload.payload))
-        expect(PayloadBuilder.omitStorageMeta(result)).toEqual(payload.payload)
+        expect(await PayloadBuilder.dataHash(result)).toEqual(await PayloadBuilder.dataHash(payload))
+        expect(await PayloadBuilder.hash(result)).toEqual(await PayloadBuilder.hash(payload))
+        expect(PayloadBuilder.omitStorageMeta(result)).toEqual(payload)
       }
     })
   })
   // NOTE: Skipped because memory DB re-used by all tests
   // causing these tests to be non-deterministic and fail
   describe.skip('next', () => {
-    const payloads: BoundWitnessWrapper | PayloadWrapper[] = []
+    const payloads: Payload[] = []
     beforeAll(async () => {
       for (let i = 0; i < 10; i++) {
-        const payload1 = { nonce: Date.now(), schema: 'network.xyo.debug' }
-        const payloadWrapper1 = PayloadWrapper.wrap(payload1)
+        const payload1: Payload = new PayloadBuilder({ schema: 'network.xyo.debug' }).fields({ nonce: Date.now() }).build()
         await delay(2)
-        const payload2 = { nonce: Date.now(), schema: 'network.xyo.test' }
-        const payloadWrapper2 = PayloadWrapper.wrap(payload2)
+        const payload2: Payload = new PayloadBuilder({ schema: 'network.xyo.test' }).fields({ nonce: Date.now() }).build()
         await delay(2)
         const signer = await Account.random()
         const boundWitness = (await new BoundWitnessBuilder()
-          .payloads([payloadWrapper1, payloadWrapper2].map(p => p.payload))
+          .payloads([payload1, payload2])
           .signer(signer)
           .build())[0]
-        const boundWitnessWrapper = BoundWitnessWrapper.parse(boundWitness, [payload1, payload2])
-        await archivist.insert([boundWitnessWrapper.payload])
+        await archivist.insert([boundWitness])
         await delay(2)
-        await archivist.insert([payloadWrapper1.payload])
+        await archivist.insert([payload1])
         await delay(2)
-        await archivist.insert([payloadWrapper2.payload])
+        await archivist.insert([payload2])
         await delay(2)
-        payloads.push(boundWitnessWrapper, payloadWrapper1, payloadWrapper2)
+        payloads.push(boundWitness, payload1, payload2)
       }
     })
     describe('desc', () => {
@@ -146,10 +134,9 @@ describe.runIf(hasMongoDBConfig())('Archivist', () => {
           expect(results).toBeArrayOfSize(expected.length)
           for (const [i, result] of results.reverse().entries()) {
             const payload = expected[i]
-            expect(await PayloadBuilder.dataHash(result)).toEqual(await payload.dataHash())
-            expect(await PayloadBuilder.dataHash(result)).toEqual(await PayloadBuilder.dataHash(payload.payload))
-            expect(await PayloadBuilder.hash(result)).toEqual(await PayloadBuilder.hash(payload.payload))
-            expect(result).toEqual(payload.payload)
+            expect(await PayloadBuilder.dataHash(result)).toEqual(await PayloadBuilder.dataHash(payload))
+            expect(await PayloadBuilder.hash(result)).toEqual(await PayloadBuilder.hash(payload))
+            expect(result).toEqual(payload)
           }
         })
       })
