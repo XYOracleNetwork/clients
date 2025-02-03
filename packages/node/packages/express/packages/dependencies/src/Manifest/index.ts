20f13d3528bb2fa888c959acb81d32cf8a3a5221
Joel Carter <JoelBCarter@users.noreply.github.com>
2025-02-03 08:32:24 -0600

    Remove child manifest

--- packages/node/packages/express/packages/dependencies/src/Manifest/index.ts
+++ packages/node/packages/express/packages/dependencies/src/Manifest/index.ts
@@ -1,6 +1,5 @@
 import type { ModuleManifest, PackageManifestPayload } from '@xyo-network/manifest-model'
 
-import chain from './chain.json' assert { type: 'json' }
 import node from './node.json' assert { type: 'json' }
 
 /**
@@ -9,5 +8,4 @@ import node from './node.json' assert { type: 'json' }
 export const defaultNode = node as PackageManifestPayload
 
 // Public children
-const chainManifest = chain as PackageManifestPayload
-export const manifestPublicChildren = [...chainManifest.nodes] as ModuleManifest[]
+export const manifestPublicChildren = [] as ModuleManifest[]
