4317129b19c9577556d8face9190d558f4de0144
Arie Trouw <arie.trouw@xylabs.com>
2025-03-12 10:55:48 -0700

    Deploy
    3.4.0

--- packages/node/packages/express/packages/test/src/testUtil/Payload/getNewPayload.ts
+++ packages/node/packages/express/packages/test/src/testUtil/Payload/getNewPayload.ts
@@ -2,11 +2,10 @@ import { PayloadBuilder } from '@xyo-network/payload-builder'
 import type { Payload } from '@xyo-network/payload-model'
 import { v4 as uuid } from 'uuid'
 
-import { knownPayloadPromise } from './getKnownPayload.js'
+import { knownPayload } from './getKnownPayload.js'
 import { schema } from './schema.js'
 
 export const getNewPayload = (): Payload => {
-  const knownPayload = knownPayloadPromise
   const fields = { ...knownPayload, uid: uuid() }
   const result = new PayloadBuilder({ schema }).fields(fields).build()
   return result
