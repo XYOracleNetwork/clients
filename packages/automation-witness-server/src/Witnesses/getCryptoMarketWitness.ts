e74072d294c35aa63ad689196c957f6a59f8abd0
Joel Carter <JoelBCarter@users.noreply.github.com>
2025-04-06 15:50:30 -0500

    Conditional uniswap witness

--- packages/automation-witness-server/src/Witnesses/getCryptoMarketWitness.ts
+++ packages/automation-witness-server/src/Witnesses/getCryptoMarketWitness.ts
@@ -13,25 +13,31 @@ import { getAccount, WalletPaths } from '../Account/index.js'
 import { getProvider } from '../Providers/index.js'
 import type { WitnessProvider } from './WitnessProvider.js'
 
+let witnesses: AttachableWitnessInstance[] | undefined
+
+const useUniswapWitness = false
+
 export const getCryptoMarketWitness: WitnessProvider<Provider> = async (provider = getProvider()): Promise<AttachableWitnessInstance[]> => {
-  const witnesses: AttachableWitnessInstance[] = [
-    await CoingeckoCryptoMarketWitness.create({
+  if (witnesses) return witnesses
+  else {
+    const coingeckoWitness = await CoingeckoCryptoMarketWitness.create({
       account: await getAccount(WalletPaths.CryptoMarket.Witness.Coingecko),
       config: {
         coins: defaultCoins,
         currencies: defaultCurrencies,
         schema: CoingeckoCryptoMarketWitnessConfigSchema,
       },
-    }),
-    await UniswapCryptoMarketWitness.create({
-      account: await getAccount(WalletPaths.CryptoMarket.Witness.Uniswap),
-      config: {
-        pools: UniswapPoolContracts,
-        schema: UniswapCryptoMarketWitnessConfigSchema,
-      },
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      provider: provider as any,
-    }),
-  ]
-  return witnesses
+    })
+    const uniswapWitness = await UniswapCryptoMarketWitness.create({
+        account: await getAccount(WalletPaths.CryptoMarket.Witness.Uniswap),
+        config: {
+          pools: UniswapPoolContracts,
+          schema: UniswapCryptoMarketWitnessConfigSchema,
+        },
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        provider: provider as any,
+      }),
+      witnesses = useUniswapWitness ? [coingeckoWitness, uniswapWitness] : [coingeckoWitness]
+    return witnesses
+  }
 }
