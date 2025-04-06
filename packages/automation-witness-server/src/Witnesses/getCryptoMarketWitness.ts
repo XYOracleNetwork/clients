326c9578de1d02cb7cbaea8b35cf82d04dcdb05d
Joel Carter <JoelBCarter@users.noreply.github.com>
2025-04-06 18:59:53 -0500

    Return uniswap witness

--- packages/automation-witness-server/src/Witnesses/getCryptoMarketWitness.ts
+++ packages/automation-witness-server/src/Witnesses/getCryptoMarketWitness.ts
@@ -9,35 +9,27 @@ import { UniswapCryptoMarketWitness, UniswapPoolContracts } from '@xyo-network/u
 import type { AttachableWitnessInstance } from '@xyo-network/witness-model'
 import type { Provider } from 'ethers'
 
-import { getAccount, WalletPaths } from '../Account/index.js'
-import { getProvider } from '../Providers/index.js'
-import type { WitnessProvider } from './WitnessProvider.js'
-
-let witnesses: AttachableWitnessInstance[] | undefined
-
-const useUniswapWitness = false
+import { getAccount, WalletPaths } from '../Account/index.ts'
+import { getProvider } from '../Providers/index.ts'
+import type { WitnessProvider } from './WitnessProvider.ts'
 
 export const getCryptoMarketWitness: WitnessProvider<Provider> = async (provider = getProvider()): Promise<AttachableWitnessInstance[]> => {
-  if (witnesses) return witnesses
-  else {
-    const coingeckoWitness = await CoingeckoCryptoMarketWitness.create({
+  return [
+    await CoingeckoCryptoMarketWitness.create({
       account: await getAccount(WalletPaths.CryptoMarket.Witness.Coingecko),
       config: {
         coins: defaultCoins,
         currencies: defaultCurrencies,
         schema: CoingeckoCryptoMarketWitnessConfigSchema,
       },
-    })
-    const uniswapWitness = await UniswapCryptoMarketWitness.create({
-        account: await getAccount(WalletPaths.CryptoMarket.Witness.Uniswap),
-        config: {
-          pools: UniswapPoolContracts,
-          schema: UniswapCryptoMarketWitnessConfigSchema,
-        },
-        // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        provider: provider as any,
-      }),
-      witnesses = useUniswapWitness ? [coingeckoWitness, uniswapWitness] : [coingeckoWitness]
-    return witnesses
-  }
+    }),
+    await UniswapCryptoMarketWitness.create({
+      account: await getAccount(WalletPaths.CryptoMarket.Witness.Uniswap),
+      config: {
+        pools: UniswapPoolContracts,
+        schema: UniswapCryptoMarketWitnessConfigSchema,
+      },
+      provider,
+    }),
+  ]
 }
