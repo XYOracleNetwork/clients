5ce0ac14dc12bb0abdc257ed1d50eecf78344ca4
Joel Carter <JoelBCarter@users.noreply.github.com>
2025-04-22 18:54:32 -0500

    Sample tx send

---
+++ packages/automation-witness-server/src/Chain/sendTransaction.ts
@@ -0,0 +1,5 @@
+import type { Payload } from '@xyo-network/payload-model'
+
+export const sendTransaction = async (elevatedPayloads: Payload[], additionalPayloads: Payload[]): Promise<void> => {
+  await Promise.resolve()
+}
