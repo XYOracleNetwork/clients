3558468da1a30a74b70c5fa95feef52b27be6b49
Joel Carter <JoelBCarter@users.noreply.github.com>
2025-04-22 18:54:43 -0500

    Submit TX for Stock Prices

--- packages/automation-witness-server/src/Jobs/TZeroStockMarket/getTask.ts
+++ packages/automation-witness-server/src/Jobs/TZeroStockMarket/getTask.ts
@@ -1,5 +1,6 @@
 import { getDefaultLogger } from '@xylabs/express'
 
+import { sendTransaction } from '../../Chain/index.ts'
 import { reportStockPrice } from './reportStockPrices.ts'
 
 export const getTask = () => {
@@ -7,8 +8,11 @@ export const getTask = () => {
   const task = async () => {
     try {
       logger.log('Reporting TZero Stock Prices')
-      await reportStockPrice('XYLB')
+      const result = await reportStockPrice('XYLB')
       logger.log('Reported TZero Stock Prices')
+      logger.log('Submit Transaction of TZero Stock Prices')
+      await sendTransaction([], result)
+      logger.log('Submitted Transaction of TZero Stock Prices')
     } catch (error) {
       logger.error(error)
     }
