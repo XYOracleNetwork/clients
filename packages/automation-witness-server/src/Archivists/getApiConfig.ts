664e9b6dddedb9db7a2c4d2ffa6502ec5267ec60
Joel Carter <JoelBCarter@users.noreply.github.com>
2025-04-22 08:42:50 -0500

    Support multiple API Configs

--- packages/automation-witness-server/src/Archivists/getApiConfig.ts
+++ packages/automation-witness-server/src/Archivists/getApiConfig.ts
@@ -1,5 +1,16 @@
 import type { ApiConfig } from '@xyo-network/api-models'
 
-export const getApiConfig = (): ApiConfig => {
+export const getStorageArchivistApiConfig = (): ApiConfig => {
   return { apiDomain: process.env.ARCHIVIST_API_DOMAIN || 'https://beta.api.archivist.xyo.network' }
+}
+
+export const getChainArchivistApiConfig = (): ApiConfig => {
+  return { apiDomain: process.env.CHAIN_SUBMISSIONS_ARCHIVIST_API_DOMAIN || 'https://beta.api.chain.xyo.network/' }
+}
+
+export const getApiConfigs = (): ApiConfig[] => {
+  return [
+    getStorageArchivistApiConfig(),
+    getChainArchivistApiConfig(),
+  ]
 }
