d0866aa6cd99de8964d1ad00c89b7f6eddf916ad
Joel Carter <JoelBCarter@users.noreply.github.com>
2025-04-22 19:04:19 -0500

    Ignore placeholder arguments

--- packages/automation-witness-server/src/Chain/sendTransaction.ts
+++ packages/automation-witness-server/src/Chain/sendTransaction.ts
@@ -1,5 +1,5 @@
 import type { Payload } from '@xyo-network/payload-model'
 
-export const sendTransaction = async (elevatedPayloads: Payload[], additionalPayloads: Payload[]): Promise<void> => {
+export const sendTransaction = async (_elevatedPayloads: Payload[], _additionalPayloads: Payload[]): Promise<void> => {
   await Promise.resolve()
 }
