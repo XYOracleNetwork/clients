6ee48c96a01e501e0d348da3e84b6dcfc08f2cfa
Joel Carter <JoelBCarter@users.noreply.github.com>
2025-04-24 07:14:06 -0500

    Wallet from ENV

--- packages/automation-witness-server/src/Chain/sendTransaction.ts
+++ packages/automation-witness-server/src/Chain/sendTransaction.ts
@@ -1,4 +1,5 @@
-import { Account } from '@xyo-network/account'
+import { assertEx } from '@xylabs/assert'
+import { HDWallet } from '@xyo-network/account'
 import type { Signed } from '@xyo-network/boundwitness-model'
 import type { Payload } from '@xyo-network/payload-model'
 import type { AllowedBlockPayload, TransactionBoundWitness } from '@xyo-network/xl1-model'
@@ -8,9 +9,12 @@ import {
   MemoryXyoSigner, XyoRunnerRpcSchemas,
 } from '@xyo-network/xl1-rpc'
 
+const accountPath = "m/44'/60'/0'/0/0" as const
+
 const getProvider = async (): Promise<MemoryXyoProvider> => {
+  const phrase = assertEx(process.env.XYO_WALLET_PHRASE, () => 'XYO_WALLET_PHRASE is not set')
+  const account = await HDWallet.fromPhrase(phrase, accountPath)
   const rpcUrl = process.env.XYO_CHAIN_RPC_URL || 'http://localhost:3000'
-  const account = await Account.random()
   const transport = new HttpRpcTransport(rpcUrl, XyoRunnerRpcSchemas)
   const runner = new JsonRpcXyoRunner(transport)
   const signer = new MemoryXyoSigner(account)
