0078bb6dc19d65d442ba48d74e84013c11a99a1f
Joel Carter <JoelBCarter@users.noreply.github.com>
2025-04-24 07:18:51 -0500

    Modularize RPC composure

--- packages/automation-witness-server/src/Chain/sendTransaction.ts
+++ packages/automation-witness-server/src/Chain/sendTransaction.ts
@@ -1,8 +1,10 @@
 import { assertEx } from '@xylabs/assert'
+import type { AccountInstance } from '@xyo-network/account'
 import { HDWallet } from '@xyo-network/account'
 import type { Signed } from '@xyo-network/boundwitness-model'
 import type { Payload } from '@xyo-network/payload-model'
 import type { AllowedBlockPayload, TransactionBoundWitness } from '@xyo-network/xl1-model'
+import type { RpcTransport } from '@xyo-network/xl1-rpc'
 import {
   HttpRpcTransport,
   JsonRpcXyoRunner, MemoryXyoProvider,
@@ -11,13 +13,23 @@ import {
 
 const accountPath = "m/44'/60'/0'/0/0" as const
 
-const getProvider = async (): Promise<MemoryXyoProvider> => {
+const getAccount = async (): Promise<AccountInstance> => {
   const phrase = assertEx(process.env.XYO_WALLET_PHRASE, () => 'XYO_WALLET_PHRASE is not set')
   const account = await HDWallet.fromPhrase(phrase, accountPath)
+  return account
+}
+
+const getRpcTransport = (): RpcTransport<typeof XyoRunnerRpcSchemas> => {
   const rpcUrl = process.env.XYO_CHAIN_RPC_URL || 'http://localhost:3000'
   const transport = new HttpRpcTransport(rpcUrl, XyoRunnerRpcSchemas)
-  const runner = new JsonRpcXyoRunner(transport)
+  return transport
+}
+
+const getProvider = async (): Promise<MemoryXyoProvider> => {
+  const account = await getAccount()
   const signer = new MemoryXyoSigner(account)
+  const transport = getRpcTransport()
+  const runner = new JsonRpcXyoRunner(transport)
   const provider = new MemoryXyoProvider({ runner, signer })
   return provider
 }
