a20222e010353ed09bca66b648d48963acfb5b47
Joel Carter <JoelBCarter@users.noreply.github.com>
2025-04-24 08:16:26 -0500

    Conditional chain witnessing

--- packages/automation-witness-server/src/Chain/sendTransaction.ts
+++ packages/automation-witness-server/src/Chain/sendTransaction.ts
@@ -1,4 +1,3 @@
-import { assertEx } from '@xylabs/assert'
 import type { AccountInstance } from '@xyo-network/account'
 import { HDWallet } from '@xyo-network/account'
 import type { Signed } from '@xyo-network/boundwitness-model'
@@ -13,28 +12,36 @@ import {
 
 const accountPath = "m/44'/60'/0'/0/0" as const
 
-const getAccount = async (): Promise<AccountInstance> => {
-  const phrase = assertEx(process.env.XYO_WALLET_PHRASE, () => 'XYO_WALLET_PHRASE is not set')
+const getAccount = async (): Promise<AccountInstance | undefined> => {
+  const phrase = process.env.XYO_WALLET_PHRASE
+  if (!phrase) return
   const account = await HDWallet.fromPhrase(phrase, accountPath)
   return account
 }
 
-const getRpcTransport = (): RpcTransport<typeof XyoRunnerRpcSchemas> => {
-  const rpcUrl = process.env.XYO_CHAIN_RPC_URL || 'http://localhost:3000'
+const getRpcTransport = (): RpcTransport<typeof XyoRunnerRpcSchemas> | undefined => {
+  const rpcUrl = process.env.XYO_CHAIN_RPC_URL
+  if (!rpcUrl) return
   const transport = new HttpRpcTransport(rpcUrl, XyoRunnerRpcSchemas)
   return transport
 }
 
-const getProvider = async (): Promise<MemoryXyoProvider> => {
+const getProvider = async (): Promise<MemoryXyoProvider | undefined> => {
   const account = await getAccount()
+  if (!account) return
   const signer = new MemoryXyoSigner(account)
   const transport = getRpcTransport()
+  if (!transport) return
   const runner = new JsonRpcXyoRunner(transport)
   const provider = new MemoryXyoProvider({ runner, signer })
   return provider
 }
 
-export const sendTransaction = async (elevatedPayloads: AllowedBlockPayload[], additionalPayloads: Payload[]): Promise<Signed<TransactionBoundWitness>> => {
+export const sendTransaction = async (
+  elevatedPayloads: AllowedBlockPayload[],
+  additionalPayloads: Payload[],
+): Promise<Signed<TransactionBoundWitness> | undefined> => {
   const provider = await getProvider()
+  if (!provider) return
   return await provider.send(elevatedPayloads, additionalPayloads)
 }
