482c22b6b6cb020b5ce02cfb5c4ff13799056a6e
Joel Carter <JoelBCarter@users.noreply.github.com>
2025-04-24 07:03:40 -0500

    Send transaction

--- packages/automation-witness-server/src/Chain/sendTransaction.ts
+++ packages/automation-witness-server/src/Chain/sendTransaction.ts
@@ -1,5 +1,23 @@
+import { Account } from '@xyo-network/account'
+import type { Signed } from '@xyo-network/boundwitness-model'
 import type { Payload } from '@xyo-network/payload-model'
+import type { AllowedBlockPayload, TransactionBoundWitness } from '@xyo-network/xl1-model'
+import {
+  HttpRpcTransport,
+  JsonRpcXyoRunner, MemoryXyoProvider,
+  MemoryXyoSigner, XyoRunnerRpcSchemas,
+} from '@xyo-network/xl1-rpc'
 
-export const sendTransaction = async (_elevatedPayloads: Payload[], _additionalPayloads: Payload[]): Promise<void> => {
-  await Promise.resolve()
+const getProvider = async (): Promise<MemoryXyoProvider> => {
+  const account = await Account.random()
+  const transport = new HttpRpcTransport('http://localhost:3000', XyoRunnerRpcSchemas)
+  const runner = new JsonRpcXyoRunner(transport)
+  const signer = new MemoryXyoSigner(account)
+  const provider = new MemoryXyoProvider({ runner, signer })
+  return provider
+}
+
+export const sendTransaction = async (elevatedPayloads: AllowedBlockPayload[], additionalPayloads: Payload[]): Promise<Signed<TransactionBoundWitness>> => {
+  const provider = await getProvider()
+  return await provider.send(elevatedPayloads, additionalPayloads)
 }
