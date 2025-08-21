57153d0a50422479e7a18c2c23f542a5e878736c
Joel Carter <JoelBCarter@users.noreply.github.com>
2025-08-21 10:58:37 -0500

    Add dataLake routes

--- packages/node/packages/express/packages/routes/src/addRoutes/addRoutes.ts
+++ packages/node/packages/express/packages/routes/src/addRoutes/addRoutes.ts
@@ -4,11 +4,15 @@ import { addDomainRoutes } from './addDomainRoutes.js'
 import { addMetricsRoutes } from './addMetricsRoutes.js'
 import { addNodeRoutes } from './addNodeRoutes.js'
 import { addTempNodeRoutes } from './addTempNodeRoutes.js'
+import { archivistMiddleware } from './archivistMiddleware.js'
 
 export const addRoutes = (app: Express): Express => {
+  const { node } = app
+  const archivistModuleIdentifier = 'Chain:Finalized'
   addDomainRoutes(app)
   addTempNodeRoutes(app)
   addMetricsRoutes(app)
+  app.use('/dataLake', archivistMiddleware({ node, archivistModuleIdentifier }))
   // This needs to be the last true route handler since it is
   // a catch-all for the root paths
   addNodeRoutes(app)
