c9234f94d40bc71684a42c77294112c88a1059ed
Joel Carter <JoelBCarter@users.noreply.github.com>
2025-09-23 16:48:24 -0500

    Remove note

--- packages/automation-witness-server/src/Jobs/TZeroStockMarket/getTask.ts
+++ packages/automation-witness-server/src/Jobs/TZeroStockMarket/getTask.ts
@@ -11,9 +11,6 @@ export const getTask = () => {
       const result = await reportStockPrice('XYLB')
       logger.log('Reported TZero Stock Prices')
       logger.log('Submit Transaction of TZero Stock Prices')
-      // NOTE: Create hash payload instead of non-elevated until validation passes for non-elevated
-      // const hashes = await Promise.all(result.map(p => PayloadBuilder.hash(p)))
-      // const hashPayloads = hashes.map(hash => new PayloadBuilder<HashPayload>({ schema: HashSchema }).fields({ hash }).build())
       const tx = await sendTransaction([], [...result])
       logger.log('Submitted Transaction of TZero Stock Prices', tx)
     } catch (error) {
