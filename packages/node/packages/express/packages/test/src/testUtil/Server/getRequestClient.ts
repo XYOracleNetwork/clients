0b33edf5f3f7371e7f2628f9075ce64cdc1ed1e9
Joel Carter <JoelBCarter@users.noreply.github.com>
2025-11-14 20:07:27 -0600

    Use new Axios JSON format

--- packages/node/packages/express/packages/test/src/testUtil/Server/getRequestClient.ts
+++ packages/node/packages/express/packages/test/src/testUtil/Server/getRequestClient.ts
@@ -1,11 +1,13 @@
 import { AxiosJson } from '@xylabs/axios'
+import { isDefined } from '@xylabs/typeof'
+import { Axios } from 'axios'
 
 let axios: AxiosJson
 
 export const getRequestClient = () => {
-  if (axios) return axios
+  if (isDefined(axios)) return axios
   const baseURL = process.env.API_DOMAIN
   console.log(`baseURL: ${baseURL}`)
-  axios = new AxiosJson({ baseURL })
+  axios = new Axios(AxiosJson.axiosConfig({ baseURL }))
   return axios
 }
