215f671f49dd3fbcb971d51bec1c7c3ef6df5bde
Arie Trouw <arie.trouw@xylabs.com>
2025-12-03 11:45:21 -0800

    updo

--- packages/automation-witness-server/src/launchServer.ts
+++ packages/automation-witness-server/src/launchServer.ts
@@ -1,8 +1,8 @@
-import { tryParseInt } from '@xylabs/express'
 import { config } from 'dotenv'
+import z from 'zod'
 
 import { server } from './Server/index.js'
 
 config({ quiet: true })
 
-void server(tryParseInt(process.env.APP_PORT))
+void server(z.number().int().safeParse(process.env.APP_PORT).data)
--- packages/node/packages/core/packages/app/src/configuration/transport/providers/expressHttp.ts
+++ packages/node/packages/core/packages/app/src/configuration/transport/providers/expressHttp.ts
@@ -1,12 +1,12 @@
-import { tryParseInt } from '@xylabs/express'
 import { getServer } from '@xyo-network/express-node-server'
 import type { MemoryNode } from '@xyo-network/node-memory'
+import z from 'zod'
 
 import type { NodeConfigurationFunction } from '../../../model/index.js'
 
 export const configureExpressHttpTransport: NodeConfigurationFunction = async (node: MemoryNode) => {
   // TODO: ON/OFF via presence
   // TODO: Convert port to URI to allow for local socket operation
-  const port = tryParseInt(process.env.APP_PORT) ?? 80
+  const port = z.number().int().safeParse(process.env.APP_PORT).data ?? 80
   await getServer(port, node)
 }
