e4780fe38766f0a90d442d7ace7102435c9d1aec
Joel Carter <JoelBCarter@users.noreply.github.com>
2026-03-06 11:31:57 -0600

    Coerce ENV VARs to int for APP_PORT

--- packages/node/packages/core/packages/app/src/configuration/transport/providers/expressHttp.ts
+++ packages/node/packages/core/packages/app/src/configuration/transport/providers/expressHttp.ts
@@ -7,6 +7,6 @@ import type { NodeConfigurationFunction } from '../../../model/index.js'
 export const configureExpressHttpTransport: NodeConfigurationFunction = async (node: MemoryNode) => {
   // TODO: ON/OFF via presence
   // TODO: Convert port to URI to allow for local socket operation
-  const port = z.number().int().safeParse(process.env.APP_PORT).data ?? 80
+  const port = z.coerce.number().int().safeParse(process.env.APP_PORT).data ?? 80
   await getServer(port, node)
 }
